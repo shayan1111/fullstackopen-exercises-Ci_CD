7b01ef37d886e5908ed4cefe3536d14265230112
shayan1111 <shayanrashidi009@gmail.com>
2026-08-19 17:50:13 +0330

    Adding Health Check

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
